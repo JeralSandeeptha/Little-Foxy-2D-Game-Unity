1b554ef5fb824959d565ebe3ef64554806b5c3cb
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2023-07-20 19:43:23 +0530

    Create Project
    Finalize the full project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //camera follow karanna oni
+    //ekata playerta variable ekak hadanawa
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //playerge position eka camera ekata samaana karanawa
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    /// <summary>
+    /// ////////////////////////////////////////////////////////////////this is the inheritance class////////////////////////////////////////////////
+    /// </summary>
+    /// 
+    protected Animator anim;
+    protected Rigidbody2D rb;
+    protected AudioSource death;
+     
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        death = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void JumpedOn()
+    {
+        anim.SetTrigger("Death");
+        death.Play();
+        rb.velocity = Vector2.zero;
+    }
+
+    private void Death()
+    {
+        Destroy(this.gameObject);
+    }
+
+}
